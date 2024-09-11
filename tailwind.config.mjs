284efc6c9c789f341d854a06c416e96a31e85fe2
jhangmez <jhangomez25@gmail.com>
2024-09-11 18:18:51 -0500

    Se modifico la ui

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,16 +1,28 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-    darkMode: ['class'],
-    content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-    	extend: {
-    		borderRadius: {
-    			lg: 'var(--radius)',
-    			md: 'calc(var(--radius) - 2px)',
-    			sm: 'calc(var(--radius) - 4px)'
-    		},
-    		colors: {}
-    	}
-    },
-	plugins: [require("tailwindcss-animate")],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      borderRadius: {
+        lg: 'var(--radius)',
+        md: 'calc(var(--radius) - 2px)',
+        sm: 'calc(var(--radius) - 4px)'
+      },
+      colors: {
+        primary: 'rgb(125, 250, 0)', // #7dfa00
+        background: 'rgb(51, 51, 51)', // #333333
+        onPrimary: 'rgb(51, 51, 51)', // #333333
+        secondary: 'rgb(95,237,0)',
+        'gray-100': 'rgb(240, 240, 240)', // #f0f0f0
+        'gray-200': 'rgb(220, 220, 220)', // #dcdcdc
+        'gray-300': 'rgb(190, 190, 190)' // #bebebe
+      },
+      fontFamily: {
+        title: ['Lyon Display', 'serif'],
+        body: ['Marselis Pro', 'sans-serif']
+      }
+    }
+  },
+
+  plugins: [require('tailwindcss-animate')]
 }
