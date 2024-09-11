c91c85a84b5ba854725eb97484fbf39c8ca9e604
jhangmez <jhangomez25@gmail.com>
2024-09-11 18:19:23 -0500

    Se modifico la ui v1

---
+++ src/components/react/AvatarDropdownMenu.jsx
@@ -0,0 +1,26 @@
+// components/AvatarDropdownMenu.jsx
+import { Avatar, AvatarImage, AvatarFallback } from '@/components/ui/avatar'
+import {
+  DropdownMenu,
+  DropdownMenuTrigger,
+  DropdownMenuContent,
+  DropdownMenuItem
+} from '@/components/ui/dropdown-menu'
+
+export const AvatarDropdownMenu = ({ src, alt, fallbackText }) => {
+  return (
+    <DropdownMenu>
+      <DropdownMenuTrigger>
+        <Avatar>
+          <AvatarImage src={src} alt={alt} />
+          <AvatarFallback>{fallbackText}</AvatarFallback>
+        </Avatar>
+      </DropdownMenuTrigger>
+      <DropdownMenuContent>
+        <DropdownMenuItem>Profile</DropdownMenuItem>
+        <DropdownMenuItem>Settings</DropdownMenuItem>
+        <DropdownMenuItem>Logout</DropdownMenuItem>
+      </DropdownMenuContent>
+    </DropdownMenu>
+  )
+}
