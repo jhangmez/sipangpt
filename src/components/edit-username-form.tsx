be8215e03bdbe8fe89caa3ee108ba285c1af1a4a
jhangmez <jhangomez25@gmail.com>
2024-10-28 02:04:11 -0500

    cambio menor 1

--- src/components/edit-username-form.tsx
+++ src/components/edit-username-form.tsx
@@ -19,7 +19,7 @@ import { toast } from 'sonner'
 
 const formSchema = z.object({
   username: z.string().min(2, {
-    message: 'Name must be at least 2 characters.'
+    message: 'El nombre debe tener al menos 2 caracteres.'
   })
 })
 
@@ -44,7 +44,7 @@ export default function EditUsernameForm({ setOpen }: EditUsernameFormProps) {
   function onSubmit(values: z.infer<typeof formSchema>) {
     localStorage.setItem('ollama_user', values.username)
     window.dispatchEvent(new Event('storage'))
-    toast.success('Name updated successfully')
+    toast.success('Nombre actualizado exitosamente')
   }
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
