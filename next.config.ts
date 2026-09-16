e3080b5775f13361260d16e42eefb3478a49293a
jhangmez <jhangomez25@gmail.com>
2026-09-15 23:34:41 -0500

    feat: preparando release v1.0.0

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
-};
+  output: 'standalone'
+  /* Opciones de configuración adicionales */
+}
 
-export default nextConfig;
+export default nextConfig
